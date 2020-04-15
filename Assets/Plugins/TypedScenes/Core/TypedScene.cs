9142b5bd1c6779bc3b2fb7308810d2bd97101ba2
Dalwyx <dalwyx@gmail.com>
2020-04-15 17:14:54 +0300

    Generic scene loader added to TypedScene class

--- Assets/Plugins/TypedScenes/Core/TypedScene.cs
+++ Assets/Plugins/TypedScenes/Core/TypedScene.cs
@@ -1,4 +1,6 @@
-﻿using UnityEditor;
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 namespace IJunior.TypedScenes
@@ -9,6 +11,35 @@ namespace IJunior.TypedScenes
         {
             var path = AssetDatabase.GUIDToAssetPath(guid);
             SceneManager.LoadScene(path);
+        }
+
+        protected static void LoadScene<T>(string guid, T argument)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+
+            UnityAction<Scene, Scene> handler = null;
+            handler = (from, to) =>
+            {
+                if (to.name == Path.GetFileNameWithoutExtension(path))
+                {
+                    SceneManager.activeSceneChanged -= handler;
+                    HandleSceneLoaders(argument);
+                }
+            };
+
+            SceneManager.activeSceneChanged += handler;
+            SceneManager.LoadScene(path);
+        }
+
+        private static void HandleSceneLoaders<T>(T loadingModel)
+        {
+            foreach (var rootObjects in SceneManager.GetActiveScene().GetRootGameObjects())
+            {
+                foreach (var handler in rootObjects.GetComponentsInChildren<ISceneLoadHandler<T>>())
+                {
+                    handler.OnSceneLoaded(loadingModel);
+                }
+            }
         }
     }
 }
