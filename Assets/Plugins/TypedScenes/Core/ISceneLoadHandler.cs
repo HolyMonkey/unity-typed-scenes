3e80c8bdeba86bc92993280cda0a7ac400fbbf49
Roman Sakutin <romaan27@gmail.com>
2020-04-28 10:36:23 +0300

    Update ISceneLoadHandler.cs

--- Assets/Plugins/TypedScenes/Core/ISceneLoadHandler.cs
+++ Assets/Plugins/TypedScenes/Core/ISceneLoadHandler.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace IJunior.TypedScenes
 {
-    interface ISceneLoadHandler<T>
+    public interface ISceneLoadHandler<T>
     {
         void OnSceneLoaded(T argument);
     }
